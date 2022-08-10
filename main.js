9681b137448550410bdf94a98908c6b5ab72ff4c
Sajjad-iq <rasooool59@gmail.com>
2022-08-10 20:59:18 +0300

    start fix isuues

--- main.js
+++ main.js
@@ -1,24 +1,53 @@
-var turn = "o"
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.cell'));
+    const announcer = document.querySelector('.announcer');
 
 
+    let board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+    let currentPlayer = 'X';
+    let isGameActive = true;
+
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                roundWon = true;
+                break;
+            }
+        }
+
+        if (roundWon) {
+            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+            isGameActive = false;
+            return;
+        }
+
+        if (!board.includes(''))
+            announce(TIE);
 
 
-function clickk(id) {
-    let div = document.getElementById(id)
-    var winArray = ["celling0", "celling1", "celling2", "celling3", "celling4", "celling5", "celling6", "celling7", "celling8", "celling9", "celling10"];
-
-    if (turn === "x" && div.innerHTML == '') {
-        div.innerHTML = "X"
-        turn = "o"
     }
-    else if (turn === "o" && div.innerHTML == '') {
-        div.innerHTML = "O"
-        turn = "x"
-    }
 
-}
-
-
-
-
-
+});
